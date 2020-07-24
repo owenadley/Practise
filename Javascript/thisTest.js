b0d005c7dc990657b57c7093b89a49cb1232105f
owenadley <owen.adley@gmail.com>
2020-07-24 11:06:57 -0400

    Cleaning

--- Javascript/thisTest.js
+++ Javascript/thisTest.js
@@ -87,3 +87,10 @@ console.log(addFour(1));
 console.log(false && "lol");
 console.log(1 || "R");
 console.log(1 == 'true');
+console.log("2" === 2);
+
+var a = [1,2,3];
+var b = [4,5,6];
+
+//a = a.concat(b);
+console.log(null && "foo = null");
